82ae1bd50a193b0f6d63417b1cea8437b2efd485
James Titus <jamesttitusjr@outlook.com>
2022-08-29 15:52:59 -0400

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    internal class Account
+    {
+        public int AccountNo { get; set; }
+        public string Description   { get; set; } = String.Empty;
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal Amount)
+        {
+            Balance += Amount;
+        }
+        public void Withdraw(decimal Amount)
+        {   if(Amount <= Balance)
+            {
+                Balance -= Amount;
+            } else
+            {
+                Console.WriteLine("Insufficient funds!");
+            }
+           
+        }
+        public void Transfer(decimal Amount, Account ToAccount)
+        {
+            Withdraw(Amount);
+            ToAccount.Deposit(Amount);
+        }
+
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Banking;
+
+Console.WriteLine("Banking!");
+
+Account checking = new Account();
